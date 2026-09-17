79f07fb2154c7dab344fba41c17778c55e680b68
Sujendran Reddy <ST10457761@vcconnect.edu.za>
2026-09-17 17:42:47 +0200

    Added command models to save device changes and support undo

---
+++ SmartX.Shared/Models/DeviceCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartX.Shared.Models;
+
+public sealed class DeviceCommand
+{
+    public required Guid Id { get; init; }
+
+    public required Guid SensorId { get; init; }
+
+    public required bool PreviousState { get; init; }
+
+    public required bool RequestedState { get; init; }
+
+    public required DateTimeOffset IssuedAtUtc { get; init; }
+
+    public DateTimeOffset? UndoneAtUtc { get; set; }
+}
---
+++ SmartX.Shared/Models/IssueCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartX.Shared.Requests;
+
+public sealed class IssueCommandRequest
+{
+    [Required(ErrorMessage = "Choose whether the device should be on or off.")]
+    public bool? RequestedState { get; set; }
+}
